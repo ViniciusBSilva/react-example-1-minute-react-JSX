b0c1982303cd0f861b8175a5a726e61ac0353915
Vinicius Bernardes Silva <vinicius.bsilva@hotmail.com>
2022-07-01 18:25:53 -0300

    Copy from react-example-1-minute-react

---
+++ comment_list.js
@@ -0,0 +1,73 @@
+'use strict';
+
+const e = React.createElement;
+
+class LikeButton extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            liked: false
+        };
+    }
+
+    render() {
+        if (this.state.liked) {
+            return 'You liked comment number ' + this.props.commentID;
+        }
+
+        return e(
+            'button', {
+                onClick: () => this.setState({
+                    liked: true
+                })
+            },
+            'Like'
+        );
+    }
+}
+
+class CommentList extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+
+    render() {
+        const items = [];
+        this.props.items.forEach((element, i) => {
+
+            const li = e(
+                'li', {
+                    key: i.toString()
+                },
+                [
+                    `This is the ${element} comment.`,
+                    e(LikeButton, {
+                        key: i.toString(),
+                        commentID: i + 1
+                    })
+
+                ]
+            );
+
+            items.push(li);
+
+        });
+
+        return e(
+            'ul', {},
+            items
+        );
+    }
+
+}
+
+const domContainer = document.getElementById('comment_list')
+const root = ReactDOM.createRoot(domContainer);
+
+root.render(
+    e(CommentList, {
+        items: ['first', 'second', 'third']
+    }),
+
+);

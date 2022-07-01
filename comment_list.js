a6456461d8b6e87dd5d081d44c48095a9a67117c
Vinicius Bernardes Silva <vinicius.bsilva@hotmail.com>
2022-07-01 19:14:35 -0300

    First example (HTML tag SCRIPT)

--- comment_list.js
+++ comment_list.js
@@ -1,73 +1,73 @@
-'use strict';
+// 'use strict';
 
-const e = React.createElement;
+// const e = React.createElement;
 
-class LikeButton extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            liked: false
-        };
-    }
+// class LikeButton extends React.Component {
+//     constructor(props) {
+//         super(props);
+//         this.state = {
+//             liked: false
+//         };
+//     }
 
-    render() {
-        if (this.state.liked) {
-            return 'You liked comment number ' + this.props.commentID;
-        }
+//     render() {
+//         if (this.state.liked) {
+//             return 'You liked comment number ' + this.props.commentID;
+//         }
 
-        return e(
-            'button', {
-                onClick: () => this.setState({
-                    liked: true
-                })
-            },
-            'Like'
-        );
-    }
-}
+//         return e(
+//             'button', {
+//                 onClick: () => this.setState({
+//                     liked: true
+//                 })
+//             },
+//             'Like'
+//         );
+//     }
+// }
 
-class CommentList extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {};
-    }
+// class CommentList extends React.Component {
+//     constructor(props) {
+//         super(props);
+//         this.state = {};
+//     }
 
-    render() {
-        const items = [];
-        this.props.items.forEach((element, i) => {
+//     render() {
+//         const items = [];
+//         this.props.items.forEach((element, i) => {
 
-            const li = e(
-                'li', {
-                    key: i.toString()
-                },
-                [
-                    `This is the ${element} comment.`,
-                    e(LikeButton, {
-                        key: i.toString(),
-                        commentID: i + 1
-                    })
+//             const li = e(
+//                 'li', {
+//                     key: i.toString()
+//                 },
+//                 [
+//                     `This is the ${element} comment.`,
+//                     e(LikeButton, {
+//                         key: i.toString(),
+//                         commentID: i + 1
+//                     })
 
-                ]
-            );
+//                 ]
+//             );
 
-            items.push(li);
+//             items.push(li);
 
-        });
+//         });
 
-        return e(
-            'ul', {},
-            items
-        );
-    }
+//         return e(
+//             'ul', {},
+//             items
+//         );
+//     }
 
-}
+// }
 
-const domContainer = document.getElementById('comment_list')
-const root = ReactDOM.createRoot(domContainer);
+// const domContainer = document.getElementById('comment_list')
+// const root = ReactDOM.createRoot(domContainer);
 
-root.render(
-    e(CommentList, {
-        items: ['first', 'second', 'third']
-    }),
+// root.render(
+//     e(CommentList, {
+//         items: ['first', 'second', 'third']
+//     }),
 
-);
+// );
